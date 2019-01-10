2da95dd32cb365dd460b9c6a3546007982622b01
Matt <MVSucharski@gmail.com>
2019-01-10 09:19:51 -0500

    part 1 of question 2 complete

--- add/add.ts
+++ add/add.ts
@@ -1,12 +1,13 @@
 function add(param1: number, param2: number): number {
-
+    const added = param1 + param2
+    return added
 }
 
 // function add2(param1: number[]): number {
 
 // }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+ console.log(add(1, 2));
+ console.log(add(3, 2));
 
 // console.log(add2(1,2,3,4,5));
 // console.log(add2(2,3));
