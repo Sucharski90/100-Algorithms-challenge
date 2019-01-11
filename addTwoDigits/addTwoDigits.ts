08788f5931d24d934407d5093567032063eff54c
Matt <MVSucharski@gmail.com>
2019-01-11 09:54:36 -0500

    add two integers done

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,9 @@
 function addTwoDigits(n: any): number {
+    const number = n.toString().split('');
 
+    return number.reduce((a, b) => {
+        return parseInt(a) + parseInt(b);
+    });
 }
 
-// console.log(addTwoDigits(29));
+ console.log(addTwoDigits(29));
