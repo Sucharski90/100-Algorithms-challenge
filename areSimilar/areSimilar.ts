df832df4e1bf8290b1141d83d75b9f6e4947526d
Matt <MVSucharski@gmail.com>
2019-01-12 23:28:28 -0500

    compare array

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,7 +1,10 @@
 function areSimilar(a: number[], b: number[]): boolean {
-   
+   let stringA = a.toString();
+   let stringB = b.toString();
+
+   return true ? stringA === stringB : false;
 }
 
 console.log(areSimilar([1, 2, 3], [1, 2, 3]));
 console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([1, 2, 2], [1, 2, 2]));
