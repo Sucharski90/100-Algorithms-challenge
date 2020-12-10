a59d7ea80f395681c1b37d637cf31d6d8e361f86
Matt <MVSucharski@gmail.com>
2020-12-09 22:55:05 -0500

    restarting

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -6,5 +6,5 @@
 
  console.log(absoluteValuesSumMinimization([2, 4, 7]));
  console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
